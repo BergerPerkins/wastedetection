90497bed36364ef72ff1337726fb30530f2b2d44
Berger <your_email_address@example.com>
2023-09-23 00:16:27 +0530

    upto Note book added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'wasteDetection',
+    version= '0.0.0',
+    author= 'Berger Perkins',
+    author_email= 'perkinsberger@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
